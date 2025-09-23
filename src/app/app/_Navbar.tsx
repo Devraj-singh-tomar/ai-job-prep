f3a2544d2bf029abcb729520b8963841ad54e531
Devraj-singh-tomar <devrajsingh.t11@gmail.com>
2025-09-23 05:30:34 +0530

    feat: add navbar and user profile component

---
+++ src/app/app/_Navbar.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { ThemeToggle } from "@/components/ThemeToggle";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { UserAvatar } from "@/components/UserAvatar";
+import { SignOutButton, useClerk } from "@clerk/nextjs";
+import {
+  BookOpenIcon,
+  BrainCircuitIcon,
+  BrainIcon,
+  FileSlidersIcon,
+  LogOut,
+  SpeechIcon,
+  User,
+} from "lucide-react";
+import Link from "next/link";
+import { useParams, usePathname } from "next/navigation";
+
+const navLinks = [
+  { name: "Interviews", href: "interviews", Icon: SpeechIcon },
+  { name: "Questions", href: "questions", Icon: BookOpenIcon },
+  { name: "Resume", href: "resume", Icon: FileSlidersIcon },
+];
+
+export function Navbar({ user }: { user: { name: string; imageUrl: string } }) {
+  const { openUserProfile } = useClerk();
+  const { jobInfoId } = useParams();
+  const pathName = usePathname();
+
+  return (
+    <nav className="h-header border-b">
+      <div className="container flex h-full items-center justify-between">
+        <Link href="/app" className="flex items-center gap-2">
+          <BrainIcon className="size-8 text-primary" />
+          <span className="text-xl font-bold">PrepAI</span>
+        </Link>
+
+        <div className="flex items-center gap-4">
+          {typeof jobInfoId === "string" &&
+            navLinks.map(({ name, href, Icon }) => {
+              const hrefPath = `/app/job-infos/${jobInfoId}/${href}`;
+
+              return (
+                <Button
+                  variant={pathName === hrefPath ? "secondary" : "ghost"}
+                  key={name}
+                  asChild
+                  className="cursor-pointer max-sm:hidden"
+                >
+                  <Link href={hrefPath}>
+                    <Icon />
+                    {name}
+                  </Link>
+                </Button>
+              );
+            })}
+
+          <ThemeToggle />
+
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <UserAvatar user={user} />
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent align="end" className="w-56">
+              <DropdownMenuItem onClick={() => openUserProfile()}>
+                <User className="mr-2" />
+                Profile
+              </DropdownMenuItem>
+
+              <SignOutButton>
+                <DropdownMenuItem>
+                  <LogOut className="mr-2" />
+                  Logout
+                </DropdownMenuItem>
+              </SignOutButton>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+    </nav>
+  );
+}
